4921f2c4042492223cf26ceb0bb88854788741fc
AliOding12 <aliz17043@gmail.com>
2024-11-17 10:00:00 +0500

    Add unit tests for classification module

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -13,4 +13,4 @@ def test_data_loader():
     data_loader = get_data_loader('data/classification', batch_size=2, train=True)
     inputs, labels = next(iter(data_loader))
     assert inputs.shape == (2, 3, 224, 224)
-    assert labels.shape == (2,)
+    assert labels.shape == (2,)# Add unit tests for classification module
